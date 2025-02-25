ea6afc6fe21bed2b0c09b282e1fb2eb00028e769
fonsinho <flores.fernandez.juan.francisco@alumnat.copernic.cat>
2025-02-25 10:49:03 +0100

    Funcionaitat de creacio de producte amb estils. Ref: #8

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\WineTypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,5 @@ Route::get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('/products', ProductController::class);
+    Route::apiResource('/winetypes', WineTypeController::class);
 });
