4cd5167787c8aad8f32f00b4c6e6e78677ace851
gonzalez.miranda.pol <gonzalez.miranda.pol@alumnat.copernic.cat>
2025-03-05 11:33:39 +0100

    Añadida funcionalidad de editar y borrar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Main from "@pages/MainPage";
 import Seller from "@pages/Seller/SellerDashboardPage";
 import Create from "@pages/CreatePage";
 import ViewProductPage from "./pages/Seller/ViewProductPage";
+import EditProductPage from "./pages/Seller/EditProductPage";
 
 export default function App() {
   
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/" element={<Main />} />
         <Route path="/seller/:id/products" element={<Seller />} />
         <Route path="/seller/:id/products/:id" element={<ViewProductPage />} />
+        <Route path="/seller/:id/products/:id/edit" element={<EditProductPage />} />
         <Route path="/create" element={<Create />} />
       </Routes>
     </Router>
