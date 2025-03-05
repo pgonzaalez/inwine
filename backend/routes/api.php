79e84e17fe47739c99d794033729786bed83259b
hache2212 <hrdomenech@gmail.com>
2025-03-05 09:40:07 +0100

    Implementacio sanctum

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,12 +8,12 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('/products', ProductController::class);
