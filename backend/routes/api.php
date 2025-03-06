5278642fbe9ae040be454450378968b816a58f32
hache2212 <hrdomenech@gmail.com>
2025-03-06 10:16:44 +0100

    User controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,13 +4,16 @@ use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\WineTypeController;
 use App\Http\Controllers\Api\InvestorController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/user', [UserController::class, 'show'])->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
