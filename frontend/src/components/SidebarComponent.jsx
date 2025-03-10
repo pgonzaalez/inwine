43b67c9bc64b309a24ee053632297f212f1878d6
fonsinho <flores.fernandez.juan.francisco@alumnat.copernic.cat>
2025-03-10 11:39:29 +0100

    ficar logica del logout

--- frontend/src/components/SidebarComponent.jsx
+++ frontend/src/components/SidebarComponent.jsx
@@ -10,6 +10,36 @@ import {
 
 export default function Sidebar() {
   const location = useLocation();
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  const logout = async () => {
+    const token = localStorage.getItem('token');
+    console.log(token); // Verifica el valor del token
+    if (!token) {
+      console.log("No hay token de autenticación");
+      return;
+    }
+
+    try {
+      const response = await fetch(`${apiUrl}/logout`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}` // Asegúrate de usar backticks
+        }
+      });
+
+      if (response.ok) {
+        // Si el logout es exitoso, puedes eliminar el token del localStorage
+        localStorage.removeItem('token');
+        window.location.href = "/";
+      } else {
+        console.log("Error al hacer logout");
+      }
+    } catch (error) {
+      console.error("Error en la solicitud de logout:", error);
+    }
+  };
 
   return (
     <>
@@ -29,44 +59,39 @@ export default function Sidebar() {
           <Divider />
           <Link
             to="/create"
-            className={`flex items-center gap-4 p-3 rounded-[20px] cursor-pointer ${
-              location.pathname === "/create"
-                ? "bg-[#efefef]"
-                : "hover:bg-gray-100"
-            }`}
+            className={`flex items-center gap-4 p-3 rounded-[20px] cursor-pointer ${location.pathname === "/create"
+              ? "bg-[#efefef]"
+              : "hover:bg-gray-100"
+              }`}
           >
             <BookmarkPlusIcon size={20} />
             <span>Pujar Producte</span>
           </Link>
           <Link
             to="/seller/123/products"
-            className={`p-3 flex items-center gap-4 rounded-[20px] cursor-pointer ${
-              location.pathname === "/seller/123/products"
-                ? "bg-[#efefef]"
-                : "hover:bg-gray-100"
-            }`}
+            className={`p-3 flex items-center gap-4 rounded-[20px] cursor-pointer ${location.pathname === "/seller/123/products"
+              ? "bg-[#efefef]"
+              : "hover:bg-gray-100"
+              }`}
           >
             <LucideShoppingCart size={20} />
             <span>Productes</span>
           </Link>
           <Link
             to="/seller/123/notificacions"
-            className={`p-3 flex items-center gap-4 rounded-[20px] cursor-pointer ${
-              location.pathname === "/seller/123/notificacions"
-                ? "bg-[#efefef]"
-                : "hover:bg-gray-100"
-            }`}
+            className={`p-3 flex items-center gap-4 rounded-[20px] cursor-pointer ${location.pathname === "/seller/123/notificacions"
+              ? "bg-[#efefef]"
+              : "hover:bg-gray-100"
+              }`}
           >
             <Bell size={20} />
             <span>Notificacions</span>
           </Link>
           <Link
-            to="/logout"
-            className={`p-3 flex items-center gap-4 rounded-[20px] cursor-pointer ${
-              location.pathname === "/logout"
-                ? "bg-[#efefef]"
-                : "hover:bg-gray-100"
-            }`}
+            to="#"
+            onClick={logout}
+            className={`p-3 flex items-center gap-4 rounded-[20px] cursor-pointer ${location.pathname === "/logout" ? "bg-[#efefef]" : "hover:bg-gray-100"
+              }`}
           >
             <LogOut size={20} />
             <span>Tanca sessió</span>
@@ -78,40 +103,36 @@ export default function Sidebar() {
       <nav className="md:hidden fixed bottom-0 left-0 right-0 bg-white shadow flex justify-around p-2 z-10">
         <Link
           to="/create"
-          className={`flex flex-col items-center p-2 ${
-            location.pathname === "/create" ? "text-[#800020]" : "text-gray-500"
-          }`}
+          className={`flex flex-col items-center p-2 ${location.pathname === "/create" ? "text-[#800020]" : "text-gray-500"
+            }`}
         >
           <BookmarkPlusIcon size={20} />
           <span className="text-xs">Pujar</span>
         </Link>
         <Link
           to="/seller/123/products"
-          className={`flex flex-col items-center p-2 ${
-            location.pathname === "/seller/123/products"
-              ? "text-[#800020]"
-              : "text-gray-500"
-          }`}
+          className={`flex flex-col items-center p-2 ${location.pathname === "/seller/123/products"
+            ? "text-[#800020]"
+            : "text-gray-500"
+            }`}
         >
           <LucideShoppingCart size={20} />
           <span className="text-xs">Productes</span>
         </Link>
         <Link
           to="/seller/123/notificacions"
-          className={`flex flex-col items-center p-2 ${
-            location.pathname === "/seller/123/notificacions"
-              ? "text-[#800020]"
-              : "text-gray-500"
-          }`}
+          className={`flex flex-col items-center p-2 ${location.pathname === "/seller/123/notificacions"
+            ? "text-[#800020]"
+            : "text-gray-500"
+            }`}
         >
           <Bell size={20} />
           <span className="text-xs">Notificacions</span>
         </Link>
         <Link
           to="/logout"
-          className={`flex flex-col items-center p-2 ${
-            location.pathname === "/logout" ? "text-[#800020]" : "text-gray-500"
-          }`}
+          className={`flex flex-col items-center p-2 ${location.pathname === "/logout" ? "text-[#800020]" : "text-gray-500"
+            }`}
         >
           <LogOut size={20} />
           <span className="text-xs">Sortir</span>
