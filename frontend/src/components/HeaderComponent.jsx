9dda26a7d660fdd84ac8db12a7df1cb9a9b671a1
gonzalez.miranda.pol <gonzalez.miranda.pol@alumnat.copernic.cat>
2025-03-13 12:15:35 +0100

    Empezada landing page

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -3,7 +3,7 @@ import { Avatar } from "@heroui/react";
 
 export default function Header() {
   return (
-    <header className="flex items-center w-[75%] h-[60px] top-0 left-1/2 transform -translate-x-1/2 fixed z-10 bg-[#eeeeee80] backdrop-blur-[5px] border border-white/80 rounded-lg max-w-screen-xl mx-auto justify-between px-3">
+    <header className="flex items-center w-[75%] h-[60px] top-0 left-1/2 transform -translate-x-1/2 fixed z-10 bg-[#eeeeee80] backdrop-blur-[5px] rounded-lg max-w-screen-xl mx-auto justify-between px-3">
       <img src="/logo.webp" className="w-[40px] h-[40px] rounded-full" />
 
       <div className="flex gap-3 text-base text-black hidden sm:flex">
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,38 +1,96 @@
 import { Link } from "react-router-dom";
-import { useFetchUser } from "@components/FetchUser";
+import { motion } from "framer-motion";
+import Header from "@components/HeaderComponent";
+import LastInteractions from "@components/LastInteractionsComponent";
 
 export default function App() {
-  const { user, loading, error } = useFetchUser();
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-        <h1 className="text-2xl font-bold text-gray-800 mb-4">Hello</h1>
-        <Link
-          to={`/seller/${user?.id || "usuari"}/products`}
-          className="block bg-blue-500 text-white py-2 px-4 rounded-md my-2 transition duration-300 hover:bg-blue-700"
-        >
-          Go to seller
-        </Link>
-        <Link
-          to="/create"
-          className="block bg-green-500 text-white py-2 px-4 rounded-md my-2 transition duration-300 hover:bg-green-700"
-        >
-          Create a product
-        </Link>
-        <Link
-          to="/register"
-          className="block bg-purple-500 text-white py-2 px-4 rounded-md my-2 transition duration-300 hover:bg-purple-700"
-        >
-          Registra un Productor
-        </Link>
+  const fadeInLeft = {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.6 } },
+  };
 
-        <Link
-          to="/login"
-          className="block bg-yellow-500 text-white py-2 px-4 rounded-md my-2 transition duration-300 hover:bg-yellow-700"
+  const fadeInRight = {
+    hidden: { opacity: 0, x: 50 },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.6 } },
+  };
+
+  const fadeInUp = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="relative w-full h-screen overflow-hidden">
+        {/* Fons de vídeo */}
+        <video
+          loop
+          autoPlay
+          muted
+          className="absolute top-0 left-0 w-full h-full object-cover"
         >
-          Login
-        </Link>
+          <source src="hero.mp4" type="video/mp4" />
+        </video>
       </div>
-    </div>
+
+      <section className="text-center">
+        <motion.div
+          className="mx-auto p-8"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: false }}
+          variants={fadeInLeft}
+        >
+          <div className="max-w-6xl mx-auto py-12 px-6">
+            <div className="mb-16">
+              <div className="flex flex-col items-start">
+                <h2 className="text-[#741C28] text-2xl font-normal mb-2">
+                  QUI SOM
+                </h2>
+                <div className="w-full h-1 bg-[#741C28] mb-6"></div>
+              </div>
+              <p className="text-black text-lg leading-relaxed">
+                Inwine és una plataforma que permet disposar de qualsevol vi a
+                la carta dels restaurants minimitzant el risc, potenciant les
+                vendes dels cellers i creant valor pel inversors a partir de
+                quantitats molt baixes.
+              </p>
+            </div>
+
+            <div className="mt-16">
+              <div className="flex flex-col items-end">
+                <h2 className="text-[#741C28] text-2xl font-normal mb-2 text-right w-full">
+                  QUÈ OFERIM
+                </h2>
+                <div className="w-full h-1 bg-[#741C28] mb-6"></div>
+              </div>
+              <p className="text-black text-lg leading-relaxed text-right">
+                Ets un celler? Arriba a més restaurants.
+                <br />
+                Tens un restaurant? Amplia la teva carta de vins sense invertir
+                i sense risc.
+                <br />
+                Vols invertir? Inverteix amb opció a altes rendibilitats i amb
+                nul risc. Tu tries el vi i el restaurant!
+              </p>
+            </div>
+          </div>
+        </motion.div>
+      </section>
+
+      <section className=" text-center">
+        <motion.div
+          className=" mx-auto p-8"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: false }}
+          variants={fadeInUp}
+        >
+          <h2 className="text-3xl font-bold">Últimes interaccions</h2>
+          <LastInteractions />
+        </motion.div>
+      </section>
+    </>
   );
 }
