2504d76b57687ae8950e9d2c32536eb186ea4a99
juanfonsi <juanfosa1|@gmail.com>
2025-03-18 12:18:25 +0100

    fix de rutas en frontend

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -41,39 +41,39 @@ class UserSeeder extends Seeder
 
         // Crear productos de prueba
         $product = Product::factory()->create([
-            'name' => 'Vi de prova',
+            'name' => 'Vi Criança',
             'origin' => 'Catalunya',
             'year' => 2020,
             'wine_type_id' => 1,
             'description' => 'Vi de prova per a la prova',
             'price_demanded' => 1000,
             'quantity' => 1,
-            'image' => 'https://escuelaversailles.com/wp-content/uploads/tipos-de-vino.jpg',
+            'image' => '/storage/proba/caja-de-vino-tinto-toro-vinas-elias-mora-6-botellas.jpg',
             'user_id' => Seller::first()->id,
         ]);
 
         $product2 = Product::factory()->create([
-            'name' => 'Vi de prova 2',
+            'name' => 'Vi i sen va ',
             'origin' => 'Madrid',
             'year' => 2018,
             'wine_type_id' => 2,
             'description' => 'Vi de prova per a la prova',
             'price_demanded' => 100,
             'quantity' => 1,
-            'image' => 'https://tienda.pagosdelrey.com/253-thickbox_default/mucho-mas-tinto.jpg',
+            'image' => '/storage/proba/botella-rioja-enamorados.jpg',
             'user_id' => Seller::first()->id,
         ]);
 
         $product3 = Product::factory()->create([
-            'name' => 'Vi de prova 3',
+            'name' => 'Vi no vi',
             'origin' => 'França',
             'year' => 2017,
             'wine_type_id' => 3,
             'description' => 'Vi de prova per a la prova',
             'price_demanded' => 9900,
             'quantity' => 1,
-            'image' => 'https://bodegaseliasmora.com/219-thickbox_default/caja-de-vino-tinto-toro-vinas-elias-mora-6-botellas.jpg',
-            'user_id' => Seller::first()->id,
+            'image' => '/storage/proba/Botella-vino.jpeg',
+           'user_id' => Seller::first()->id,
         ]);
 
         // Crear usuarios aleatorios
