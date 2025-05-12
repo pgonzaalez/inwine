f794ed632c933441479a504273dd34e1b301007d
hache2212 <hrdomenech@gmail.com>
2025-05-12 11:11:49 +0200

    Apartat de comisions

---
+++ backend/app/Filament/Resources/CommissionResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CommissionResource\Pages;
+use App\Filament\Resources\CommissionResource\RelationManagers;
+use App\Models\Commission;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CommissionResource extends Resource
+{
+    protected static ?string $model = Commission::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-receipt-percent';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('percentage_to_seller')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('percentage_to_restaurant')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('percentage_to_investor')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('percentage_to_seller')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('percentage_to_restaurant')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('percentage_to_investor')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCommissions::route('/'),
+            'create' => Pages\CreateCommission::route('/create'),
+            'edit' => Pages\EditCommission::route('/{record}/edit'),
+        ];
+    }
+}
