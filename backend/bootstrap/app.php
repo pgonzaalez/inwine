47daee6cf560eae760c89d5963306facd728214b
Pau <martin.peralta.pau@alumnat.copernic.cat>
2026-02-12 21:50:53 +0100

    Cistella carga cuan no hi ha usuari

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,8 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => null);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+            return response()->json([
+                'message' => 'Unauthenticated.'
+            ], 401);
+        });
+    })
+    ->create();
